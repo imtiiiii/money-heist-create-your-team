cdfa4288bb8c5a2270e2a24af086a5a88c052d25
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-09-26 12:58:39 +0600

    data loading done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
       {/* /* calling header from header component folder  */}
       < Header ></Header>
+      <main>
+        <Shop></Shop>
+      </main>
     </div >
   );
 }
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,8 +1,18 @@
 // this component will hold both team member and cart details
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import "./Shop.css"
 const Shop = () => {
     // as Shop is parent of both team and cart container , the data we need from json file will be called to this parent section so that both team and cart child can get the data from their parent
+
+    //USESTATE AND USEEFFECT MUST BE USED TO GET AND SHOW DATA 
+    //HERE MEMBERS=TEAM MEAMBERS. USING USE STATE FOR TEAM MEMBERS BEACUSE WE WILL SHOW THE MEMBERS DATA SO THE STATE OF OUR PAGE CHANGEES
+    const [members, setMembers] = useState([]);
+    useEffect(() => {
+        fetch("./data.JSON")
+            .then(res => res.json())
+            .then(data => setMembers(data));
+    }, [])
+    console.log(members);
     return (
 
         <section className="container-fluid bg-ligt">
