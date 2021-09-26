a6728ee288f4b191d960f9f45708f76d0b87143a
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-09-26 22:08:10 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
     const totalBudhet = 15000;
     return (
         <header className="container-fluid  text-center header py-3">
-            <h2>Want to make your own <span className="text-danger"> Money Heist </span>team ? </h2>
-            <h4>This is your chance now !!!!</h4>
+            <h2>Want to make your own <span className=""> Money Heist </span>team ? </h2>
+            <h4 className="this-is-your-chance">This is your chance now !!!!</h4>
             <h4>Select now</h4>
             <h5> Total budghet: ${totalBudhet}</h5>
 
--- src/components/Member/Member.js
+++ src/components/Member/Member.js
@@ -1,34 +1,38 @@
 // this component will contain a single member details and return it to Members component where all team members data will be shown
-import React from 'react';
+import React, { useState } from 'react';
 import "./Member.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAd, faAddressCard, faCartArrowDown, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard, faCartArrowDown, faCoffee } from '@fortawesome/free-solid-svg-icons'
 const Member = (props) => {
     const element = <FontAwesomeIcon icon={faAddressCard} />
     const memberDetails = props.details;
-    //const memberDetailsArray = [memberDetails];
-    //console.log(memberDetails);
+
+    function Toggle(value) {
+        const [disable, setDisable] = useState(false);
+        setDisable(props.value);
+        return <button disabled={disable} >Select</button>
+    }
 
     const { name, src, role, salary, nationality, born } = memberDetails;
     return (
         <div className=" col col-4 member-details">
-            <div className="bg-white border border-danger member-card  px-4 d-flex justify-content-center flex-column align-items-center rounded-3">
+            <div className="bg-white border border-3 border-danger member-card  px-4 d-flex justify-content-center flex-column align-items-center rounded-3">
                 <div className="member-img   d-flex justify-content-center">
                     <img src={src} alt="" className="" />
                 </div>
-                <div className="pt-3">
-                    <h5>Name:{name}</h5>
+                <div className="mt-2">
+                    <h5 className="details-hover py-3 fw-bolder">Name: {name}</h5>
                     <hr />
-                    <p>Born: {born}</p>
+                    <p className="details-hover py-3">Born: {born}</p>
                     <hr />
-                    <p>Role: {role}</p>
+                    <p className="details-hover py-3">Role: {role}</p>
                     <hr />
-                    <p>Nationality: {nationality}</p>
+                    <p className="details-hover py-3">Nationality: {nationality}</p>
                     <hr />
-                    <p>Salary: ${salary}</p>
+                    <p className="details-hover py-3">Salary: ${salary}</p>
                     <hr />
                 </div>
-                <button type="button" className="btn btn-outline-danger" onClick={() => props.selectBtn({ memberDetails })}>{element} Select</button>
+                <button type="button" className="btn btn-outline-danger" onClick={() => { props.selectBtn({ memberDetails }) }}>{element} Select</button>
             </div>
         </div>
     );
