754e035240c1896c83545823f0601ce4fe68bcdf
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-09-26 12:37:37 +0600

    header done

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,20 @@
+// this component will hold both team member and cart details
+import React, { Component } from 'react';
+import "./Shop.css"
+const Shop = () => {
+    return (
+
+        <section className="container-fluid bg-ligt">
+            {/* as this container will contain two section side by side we will use grid display to divide them their needed space */}
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+
+        </section>
+    );
+};
+
+export default Shop;
