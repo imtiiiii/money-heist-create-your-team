b1104d770b6d4489862a343a33664ccc068b7f78
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-09-26 12:42:51 +0600

    working

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,14 +2,17 @@
 import React, { Component } from 'react';
 import "./Shop.css"
 const Shop = () => {
+    // as Shop is parent of both team and cart container , the data we need from json file will be called to this parent section so that both team and cart child can get the data from their parent
     return (
 
         <section className="container-fluid bg-ligt">
-            {/* as this container will contain two section side by side we will use grid display to divide them their needed space */}
-            <div>
+            {/* as this container/section (up) will contain two section side by side we will use grid display to divide them their needed space */}
+            {/* this div (down) will contain all the team member details */}
+            <div className="team-container">
 
             </div>
-            <div>
+            {/* this div (down) will contain all the cart details */}
+            <div className="cart-container">
 
             </div>
 
